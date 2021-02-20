f4b43184f7b2945af95e6429af8fd1d88b3c8e9d
Max <maxoblack@yahoo.com>
2021-02-20 23:17:24 +0000

    Create main loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "window.hpp"
 
 void load_gl();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main() {
 	Window window;
@@ -18,7 +19,19 @@ int main() {
 		return -1;
 	}
 
-	std::cout << glfwWindowShouldClose(*window) << std::endl;
+	glfwSetFramebufferSizeCallback(*window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(*window)) {
+		if(glfwGetKey(*window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(*window, true);
+		}
+
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(*window);
+		glfwPollEvents();
+	}
 
 	return 0;
 }
@@ -27,4 +40,8 @@ void load_gl() {
 	if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
 		throw std::runtime_error("Failed to initialise GLAD");
 	}
+}
+
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
