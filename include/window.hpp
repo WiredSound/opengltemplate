f7ef1c41387b2ad8cb552b9994aaa8a5ed4ebc5b
WiredSound <maxoblack@yahoo.com>
2021-02-21 16:51:31 +0000

    Document the Window class

--- include/window.hpp
+++ include/window.hpp
@@ -2,8 +2,11 @@
 
 #include <GLFW/glfw3.h>
 
+/// Wrapper around a `GLFWwindow` object. Automatically calls `glfwTerminate` on destruction.
 class Window {
 public:
+    /// Creates a window with the specified dimensions and title. Will throw `std::runtime_error` should GLFW fail to
+    /// create the window.
     void create(int width, int height, const char* title);
     GLFWwindow* operator*();
     ~Window();
